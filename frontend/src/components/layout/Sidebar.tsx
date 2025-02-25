26239904f1aa3e76df389fe17180065d4299bb49
flynnhillier <flynnhillier@gmail.com>
2025-02-25 21:37:39 +0000

    refactor sidebar & restyle;

--- frontend/src/components/layout/Sidebar.tsx
+++ frontend/src/components/layout/Sidebar.tsx
@@ -2,103 +2,91 @@ import { Circle, Heart, Home, LucideProps, Plus, Search, Text, LogIn } from "luc
 import logo from "@assets/Logo.svg";
 import { useMe } from "@/hooks/auth.hooks";
 import { Skeleton } from "../ui/skeleton";
-import { useMemo } from "react";
-import { ClassNameValue } from "tailwind-merge";
-import { cn } from "@/lib/utils";
+import { Link } from "react-router-dom";
 
 const items = [
   {
-    title: "Home",
+    text: "Home",
     url: "#",
     icon: Home,
   },
   {
-    title: "Search",
+    text: "Search",
     url: "#",
     icon: Search,
   },
   {
-    title: "Notifications",
+    text: "Notifications",
     url: "#",
     icon: Heart,
   },
   {
-    title: "Create",
+    text: "Create",
     url: "#",
     icon: Plus,
   },
   {
-    title: "More",
+    text: "More",
     url: "#",
     icon: Text,
   },
 ];
 
 function SidebarItem({
-  title,
+  text,
   url,
   Icon,
 }: {
-  title: string;
+  text: string;
   url: string;
   Icon: React.ElementType<LucideProps>;
 }) {
   return (
-    <a href={url} className="flex items-center">
+    <a href={url} className="flex items-center hover:scale-105 transition-transform duration-200">
       <span className="flex shrink-0 items-center">
         <Icon className="h-7 w-7" />
       </span>
-      <span className="text-3xl pl-5 lg:flex hidden">{title}</span>
+      <span className="text-3xl pl-5 lg:flex hidden">{text}</span>
     </a>
   );
 }
 
-function AuthenticationBanner() {
-  const { data, isLoading } = useMe();
-
-  const AuthenticationIcon = ({ className }: { className?: ClassNameValue }) => {
-    if (isLoading)
-      return <Skeleton className={cn("rounded-full w-8 h-8 inline-block", className)} />;
-    if (!data?.user?.profile_picture_url) return <LogIn className={cn("h-8 w-8", className)} />;
-
-    return (
-      <img
-        src={data?.user?.profile_picture_url}
-        alt="avatar"
-        className={cn("rounded-full w-8 h-8", className)}
-      />
-    );
-  };
-
-  const AuthenticationText = () => {
-    if (isLoading) return <Skeleton className="w-40 h-8" />;
-    if (data?.user) return data.user.username;
-    return "Log in";
-  };
-
-  const redirect = useMemo(() => {
-    if (isLoading) return false;
-    if (data?.user) return "/profile";
-
-    return "/login";
-  }, [data, isLoading]);
+function SidebarItemFooter() {
+  const { data, isPending } = useMe();
 
   return (
-    <a
-      className="flex flex-row gap-4 justify-start text-3xl items-center flex-nowrap shrink-0"
-      href={redirect || undefined}
+    <Link
+      to={data?.user ? "/profile" : "/login"}
+      className="flex items-center hover:scale-105 transition-transform duration-200 flex-row gap-2 "
     >
       <span className="flex shrink-0 items-center">
-        <AuthenticationIcon className={"w-12 h-12"} />
+        {isPending ? (
+          <Skeleton className="h-12 w-12 rounded-full" />
+        ) : !data?.user ? (
+          <LogIn className="h-8 w-8 m-2" />
+        ) : data.user.profile_picture_url ? (
+          <img
+            src={data.user.profile_picture_url}
+            alt="profile"
+            className="h-12 w-12 rounded-full"
+          />
+        ) : (
+          <Circle className="h-12 w-12" />
+        )}
       </span>
-      <AuthenticationText />
-    </a>
+      {isPending && <Skeleton className="h-7 w-44 hidden lg:inline-block" />}
+      {!isPending && (
+        <span className="text-3xl lg:flex hidden text-nowrap w-48 overflow-ellipsis  whitespace-nowrap ">
+          {data?.user ? data.user.username : "login"}
+        </span>
+      )}
+    </Link>
   );
 }
 
 const AppSidebar = () => {
   return (
-    <div className="border-r-2 border-neutral-500 w-fit h-full px-4 lg:px-8 lg:items-start items-center bg-background flex flex-col justify-between py-8 text-yellow-950">
+    <div className="border-r-2 border-neutral-500 w-fit box-border h-full lg:px-8 lg:items-start items-center bg-background flex flex-col justify-between py-8 text-yellow-950 px-4">
       <div className="flex items-center" id="logo">
         <span className="lg:w-[46px] lg:h-[46px] w-8 h-8 shrink-0">
           <img src={logo} alt="logo" />
@@ -109,12 +97,10 @@ const AppSidebar = () => {
       </div>
       <div className="h-[40%] flex flex-col justify-between">
         {items.map((item) => (
-          <SidebarItem title={item.title} url={item.url} Icon={item.icon} key={item.url} />
+          <SidebarItem text={item.text} url={item.url} Icon={item.icon} key={item.url} />
         ))}
       </div>
-      <div className="flex justify-start">
-        <AuthenticationBanner />
-      </div>
+      <SidebarItemFooter />
     </div>
   );
 };

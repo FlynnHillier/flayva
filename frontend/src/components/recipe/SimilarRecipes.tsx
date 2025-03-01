b6d57b90cda89f23fa0323eb898451e994b8e057
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2025-03-01 19:16:47 +0000

    similar recipes not looking good

--- frontend/src/components/recipe/SimilarRecipes.tsx
+++ frontend/src/components/recipe/SimilarRecipes.tsx
@@ -1,9 +1,36 @@
-export default function SimilarRecipes(props) {
-  return (
-    <div className="sticky bottom-0 h-auto min-h-40 bg-white w-full p-6 flex flex-col">
-      <h2 className="text-2xl font-bold mb-2">
-        Similar Recipes:
-      </h2>
-    </div>
-  )
+interface Recipe {
+	image: string;
+	url: string;
+}
+
+interface ImageGridProps {
+	images: Recipe[];
+}
+
+function ImageGrid({ images }: ImageGridProps) {
+	const imgGrid = images.map((recipe, index) => (
+		<div key={index} className="w-full aspect-square">
+			<img src={recipe.image} className="w-full h-full object-cover" />
+		</div>
+	));
+
+	return (
+		// Using grid-flow-col and auto-cols to ensure a single row
+		// Each image cell maintains a width between 22% and 30% of the container
+		<div className="grid grid-flow-col auto-cols-[minmax(22%,30%)] gap-2">
+			{imgGrid}
+		</div>
+	);
+}
+
+interface SimilarRecipesProps {
+	recommendedRecipes: Recipe[];
+}
+export default function SimilarRecipes(props: SimilarRecipesProps) {
+	return (
+		<div className="sticky bottom-0 h-auto min-h-40 bg-white w-full p-6 flex flex-col">
+			<h2 className="text-2xl font-bold mb-2">Similar Recipes:</h2>
+			<ImageGrid images={props.recommendedRecipes} />
+		</div>
+	);
 }

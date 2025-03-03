db9aa090625a7c69935ff9e491c41c56405f9549
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2025-03-03 20:13:26 +0000

    sidebar fixed width star rating now fixed position

--- frontend/src/components/recipe/RecipeBio.tsx
+++ frontend/src/components/recipe/RecipeBio.tsx
@@ -1,25 +1,23 @@
+import { FaStar, FaStarHalfAlt, FaRegStar } from 'react-icons/fa';
+
+interface StarRatingProps {
+	rating: number; 
+	className?: string;
+}
+
 type Bio = {
 	profilePic: string;
 	username: string;
 	rating: number;
 };
 
-import { FaStar, FaStarHalfAlt, FaRegStar } from 'react-icons/fa';
-
-interface StarRatingProps {
-	rating: number; // A double value from 0.0 to 5.0
-	className?: string; // Optional custom Tailwind classes
-}
-
 export const StarRating: React.FC<StarRatingProps> = ({
 	rating,
 	className = 'text-yellow-500 w-16 h-16',
 }) => {
-	// Calculate the number of full stars
+
 	const fullStars = Math.floor(rating);
-	// Determine if a half star is needed (for ratings with a fractional part >= 0.5)
 	const hasHalfStar = rating - fullStars >= 0.5;
-	// Calculate the remaining empty stars so that a total of 5 stars is displayed
 	const emptyStars = 5 - fullStars - (hasHalfStar ? 1 : 0);
 
 	return (
@@ -48,7 +46,7 @@ export default function RecipeBio(props: Bio) {
 			</div>
 			<div className="text-gray-500">
 				<StarRating rating={props.rating} className='text-yellow-500'/>
-				<div className="text-sm font-medium absolute ml-8">
+				<div className="text-sm font-medium relative ml-8">
 					{props.rating.toFixed(1)}
 				</div>
 			</div>
--- frontend/src/components/recipe/RecipeMain.tsx
+++ frontend/src/components/recipe/RecipeMain.tsx
@@ -1,7 +1,7 @@
 export default function RecipeMain() {
 	return (
 		<>
-			<div className="w-8/12" id="recipeMain">
+			<div className="w-full" id="recipeMain">
 				<h1>RECIPE MAIN SECTION</h1>
 			</div>
 		</>

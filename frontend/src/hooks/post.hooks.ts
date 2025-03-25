61b1c933d779ac6227aa7b1d7d4d2f23905c7365
flynnhillier <flynnhillier@gmail.com>
2025-03-25 14:48:08 +0000

    getPostById api use query-key-store; .query -> .queries

--- frontend/src/hooks/post.hooks.ts
+++ frontend/src/hooks/post.hooks.ts
@@ -1,12 +1,23 @@
 import { api } from "@/api/api";
 import { createConfigurableMutation } from "@/hooks/util/configurableMutation";
+import { queries } from "@/queries";
+import { useQuery } from "@tanstack/react-query";
 
+/**
+ * Creates a new post
+ * @returns The result of the creation
+ */
 export const useCreateNewPost = createConfigurableMutation(
   api.post.createNewPost,
   ["posts", "create"],
   {}
 );
 
+/**
+ * Deletes an existing post
+ * @param postId - The Id of the post to delete
+ * @returns The result of the deletion
+ */
 export const useDeleteExistingPost = (postId: string) =>
   createConfigurableMutation(
     async () => await api.post.deleteExistingPost(postId),
@@ -14,9 +25,9 @@ export const useDeleteExistingPost = (postId: string) =>
     {}
   );
 
-export const useGetPostById = (postId: string) =>
-  createConfigurableMutation(
-    async () => await api.post.getPostById(postId),
-    ["posts", "get", postId],
-    {}
-  );
+/**
+ * Fetches a post by its Id
+ * @param postId - The Id of the post to fetch
+ * @returns The post with the given Id
+ */
+export const useGetPostById = (postId: string) => useQuery(queries.post.getPostById(postId));
--- frontend/src/queries/index.ts
+++ frontend/src/queries/index.ts
@@ -2,8 +2,9 @@ import { mergeQueryKeys } from "@lukemorales/query-key-factory";
 
 // Import all queries from the queries folder
 import { test } from "./test";
-import { auth } from "@/queries/auth.query";
-import { social } from "@/queries/social.query";
-import { recipe } from "@queries/recipe.query";
+import { auth } from "@/queries/auth.queries";
+import { social } from "@/queries/social.queries";
+import { recipe } from "@/queries/recipe.queries";
+import { post } from "@/queries/post.queries";
 
-export const queries = mergeQueryKeys(test, auth, social, recipe);
+export const queries = mergeQueryKeys(test, auth, social, recipe, post);

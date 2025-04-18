25e90dc5767be06384d3ec1bdf25e2602c1f7b69
flynnhillier <flynnhillier@gmail.com>
2025-04-18 14:06:36 +0100

    update profile content to use post preview element

--- frontend/src/components/profile/profile-content.tsx
+++ frontend/src/components/profile/profile-content.tsx
@@ -1,32 +1,11 @@
 import { useProfile } from "@/contexts/profile.context";
 import { useInfiniteScrollProfilePostPreviews } from "@/hooks/post.hooks";
-import { uploadThingFileUrlFromKey } from "@/lib/ut";
-import { type PostPreview } from "@flayva-monorepo/shared/types";
 import { useEffect, useMemo } from "react";
-import { Link } from "react-router-dom";
 import { toast } from "sonner";
 import { useInView } from "react-intersection-observer";
 import { FourSquare } from "react-loading-indicators";
 import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
-
-const PostPreviewElement = ({ preview }: { preview: PostPreview }) => {
-  return (
-    <Link
-      to={`/p/${preview.id}`}
-      className={cn("rounded-sm overflow-hidden aspect-square w-full border-1 relative flex justify-center items-center")}
-    >
-      <span className="absolute bottom-2 left-2 text-white text-md font-bold bg-black/50 bg-opacity-50 py-1 px-1.5 z-20 rounded-sm max-w-4/5 truncate">
-        {preview.recipe.title}
-      </span>
-      <div className="absolute inset-0 bg-black opacity-0 hover:opacity-20 z-10 transition-opacity ease-in-out " />
-      <img
-        src={uploadThingFileUrlFromKey(preview.image.key)}
-        className="object-cover shrink-0 min-w-full min-h-full"
-      />
-    </Link>
-  );
-};
+import PostPreview, { PostPreviewSkeleton } from "../post/PostPreview";
 
 export const ProfileContent = () => {
   const { profile } = useProfile();
@@ -41,6 +20,7 @@ export const ProfileContent = () => {
     isFetchingNextPage,
     isFetching,
     isFetched,
+    isLoading,
   } = useInfiniteScrollProfilePostPreviews(profile.user.id);
 
   const previews = useMemo(
@@ -64,7 +44,7 @@ export const ProfileContent = () => {
   return (
     <div className="w-full max-w-7xl p-3 grid grid-cols-1 sm:grid-cols-2  md:grid-cols-3 xl:grid-cols-4  gap-3  place-items-center">
       {previews.map((preview) => (
-        <PostPreviewElement key={preview.id} preview={preview} />
+        <PostPreview key={preview.id} preview={preview} showUser={false} />
       ))}
       {
         // If there are no posts and not fetching, show a message
@@ -74,9 +54,15 @@ export const ProfileContent = () => {
           </div>
         )
       }
+
+      {isLoading &&
+        Array.from({ length: 4 }).map((_, index) => (
+          <PostPreviewSkeleton key={index} />
+        ))}
+
       {
         // If fetching, show a loading indicator
-        (isFetchingNextPage || !isFetched) && (
+        ((!isLoading && isFetchingNextPage) || !isFetched) && (
           <div className="col-span-full text-center text-gray-500">
             <FourSquare color={"gray"} size={"small"} />
           </div>

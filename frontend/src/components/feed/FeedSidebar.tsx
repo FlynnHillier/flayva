3b4d2cb0ea7a30d4c99169113f1d00ac7f91296a
jadd0 <102069567+jadd0@users.noreply.github.com>
2025-04-24 12:09:39 +0300

    - Changed PostRating so automatically false unless specified - Moved a Tags component to it's own file - Made only the FeedSidebar scrollable, not the feed itself - Sidebar now looks good

--- frontend/src/components/feed/FeedSidebar.tsx
+++ frontend/src/components/feed/FeedSidebar.tsx
@@ -1,73 +1,121 @@
-import { cn } from "@/lib/utils";
-import { Post } from "@flayva-monorepo/shared/types";
-import { useMemo } from "react";
-import { ClassNameValue } from "tailwind-merge";
-import { UserCard } from "../common/profile.common";
-import { Ingredient } from "../post/view/post-recipe-details";
+import { cn } from '@/lib/utils';
+import { Post } from '@flayva-monorepo/shared/types';
+import { useMemo } from 'react';
+import { ClassNameValue } from 'tailwind-merge';
+import { Ingredient } from '../post/view/post-recipe-details';
+import { PostRating } from '../post/ratings/ratings-common';
+import Tags from '../tags/Tags';
+import { Link } from 'react-router-dom';
+import { UserCard } from '@/components/common/profile.common';
 
 const SidebarSection = ({
-  header,
-  children,
+	header,
+	children,
 }: React.PropsWithChildren<{ header: string }>) => {
-  return (
-    <div className="flex flex-col gap-2 w-full h-fit rounded-lg">
-      <h2 className="text-xl font-semibold">{header}</h2>
-      {children}
-    </div>
-  );
+	return (
+		<div className="flex flex-col gap-2 w-full h-fit rounded-lg">
+			<h2 className="text-xl font-semibold">{header}</h2>
+			{children}
+		</div>
+	);
 };
 
 export const FeedSidebar = ({
-  post,
-  className,
-  isTransitioning = false,
+	post,
+	className,
+	isTransitioning = false,
 }: {
-  className?: ClassNameValue;
-  post: Post | undefined;
-  isTransitioning: boolean;
+	className?: ClassNameValue;
+	post: Post | undefined;
+	isTransitioning: boolean;
 }) => {
-  const isSkeleton = useMemo(
-    () => isTransitioning || !post,
-    [isTransitioning, post]
-  );
+	const isSkeleton = useMemo(
+		() => isTransitioning || !post,
+		[isTransitioning, post]
+	);
 
-  return (
-    <div
-      className={cn("border-x-2 py-7 px-4 flex flex-col space-y-4", className)}
-    >
-      <span className="Recipe text-2xl font-semibold">{post?.recipe.title}</span>
-      <div className="flex flex-col space-y-8 mt-2">
-        <SidebarSection header="Ingredients">
-          <ul className="flex flex-col space-y-1 list-disc list-inside">
-            {isSkeleton
-              ? Array.from({ length: 4 }).map((_) => (
-                  <li className="h-6 w-42 animate-pulse bg-muted rounded-full" />
-                ))
-              : post?.recipe.ingredients.map((ingredient) => (
-                  <li
-                    key={ingredient.ingredientItem.id}
-                    className="text-sm font-semibold text-primary-foreground-foreground"
-                  >
-                    <Ingredient ingredient={ingredient} />
-                  </li>
-                ))}
-          </ul>
-        </SidebarSection>
-        <SidebarSection header="Instructions">
-          <ol className="list-decimal list-inside space-y-1 pl-1">
-            {isSkeleton
-              ? Array.from({ length: 4 }).map((_) => (
-                  <li className="h-6 w-42 animate-pulse bg-muted rounded-full" />
-                ))
-              : post?.recipe.instructions.map((instruction, index) => (
-                  <li key={index} className="flex items-start gap-2 ">
-                    <span className="font-medium">{index + 1}.</span>
-                    <p className="flex-1">{instruction.instruction}</p>
-                  </li>
-                ))}
-          </ol>
-        </SidebarSection>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			className={cn(
+				'border-x-2 py-7 px-4 flex flex-col space-y-2 h-full overflow-y-auto',
+				className
+			)}
+		>
+			<UserCard user={post?.owner} />
+			<span className="Recipe text-2xl font-semibold">
+				{post?.recipe.title}
+			</span>
+			<PostRating
+				rating={post?.recipe.ratings.statiststics.average}
+				className={'mt-2'}
+			/>
+			<span className="text-sm text-muted-foreground">
+				{isSkeleton ? (
+					<span className="h-4 w-32 animate-pulse bg-muted rounded-full" />
+				) : (
+					new Date(post?.created_at ?? '').toLocaleDateString('en-GB', {
+						year: 'numeric',
+						month: 'long',
+						day: 'numeric',
+					})
+				)}
+			</span>
+			<div className="flex flex-col space-y-8 mt-2">
+				<Tags tags={post?.recipe.tags} />
+				<SidebarSection header="Ingredients">
+					<ul className="flex flex-col space-y-1 list-disc list-inside">
+						{isSkeleton
+							? Array.from({ length: 4 }).map((_) => (
+									<li className="h-6 w-42 animate-pulse bg-muted rounded-full" />
+							  ))
+							: post?.recipe.ingredients.map((ingredient) => (
+									<li
+										key={ingredient.ingredientItem.id}
+										className="text-sm font-semibold text-primary-foreground-foreground"
+									>
+										<Ingredient ingredient={ingredient} />
+									</li>
+							  ))}
+					</ul>
+				</SidebarSection>
+				<SidebarSection header="Instructions">
+					<ol className="list-decimal list-inside space-y-1 pl-1">
+						{isSkeleton
+							? Array.from({ length: 4 }).map((_) => (
+									<li className="h-6 w-42 animate-pulse bg-muted rounded-full" />
+							  ))
+							: post?.recipe.instructions.map((instruction, index) => (
+									<li key={index} className="flex items-start gap-2 ">
+										<span className="font-medium">{index + 1}.</span>
+										<p className="flex-1">{instruction.instruction}</p>
+									</li>
+							  ))}
+					</ol>
+				</SidebarSection>
+			</div>
+			<div className="flex justify-center mt-4">
+				<Link
+					to={{ pathname: `/p/${post?.id}` }}
+					className="
+            inline-block
+            px-6 py-2
+            rounded-md
+            bg-primary
+            text-white
+            font-semibold
+            transition
+            duration-150
+            hover:bg-primary-dark
+            focus:outline-none
+            focus:ring-2
+            focus:ring-primary
+            focus:ring-offset-2
+            shadow
+          "
+				>
+					View more info...
+				</Link>
+			</div>
+		</div>
+	);
 };
--- frontend/src/components/post/view/post-body.tsx
+++ frontend/src/components/post/view/post-body.tsx
@@ -1,76 +1,52 @@
-import { usePost } from "@/contexts/post.context";
-import { PostImageCarousel } from "./post-image-carousel";
-import { Skeleton } from "@/components/ui/skeleton";
-import { Post } from "@flayva-monorepo/shared/types";
-import { Badge } from "@/components/ui/badge";
-import { capitalizeFirstLetter, cn } from "@/lib/utils";
-import { ClassNameValue } from "tailwind-merge";
-import { PostLikeButton } from "./post-interactions";
-import Tag from "@/components/tags/Tag";
+import { usePost } from '@/contexts/post.context';
+import { PostImageCarousel } from './post-image-carousel';
+import { Skeleton } from '@/components/ui/skeleton';
+import { cn } from '@/lib/utils';
+import { ClassNameValue } from 'tailwind-merge';
+import { PostLikeButton } from './post-interactions';
+import Tags from '@/components/tags/Tags';
 
 function Description({
-  description,
-  className,
+	description,
+	className,
 }: {
-  description: string | undefined;
-  className?: ClassNameValue;
+	description: string | undefined;
+	className?: ClassNameValue;
 }) {
-  return (
-    <section className={cn("", className)}>
-      {description ? (
-        <p className="text-base font-normal">{description}</p>
-      ) : (
-        <Skeleton className="h-18 w-96" />
-      )}
-    </section>
-  );
-}
-
-function Tags({
-  tags,
-  className,
-}: {
-  tags: Post["recipe"]["tags"] | undefined;
-  className?: ClassNameValue;
-}) {
-  //TODO: add icons for tags based on the group / category
-
-  return (
-    <section className={cn("flex flex-row gap-1 flex-wrap", className)}>
-      {tags === undefined
-        ? Array.from({ length: 3 }, (_, i) => (
-            <Skeleton className="w-14 h-6 rounded-xl" key={i} />
-          ))
-        : tags.map((tag) => (
-            <Tag tag={tag} />
-          ))}
-    </section>
-  );
+	return (
+		<section className={cn('', className)}>
+			{description ? (
+				<p className="text-base font-normal">{description}</p>
+			) : (
+				<Skeleton className="h-18 w-96" />
+			)}
+		</section>
+	);
 }
 
 function Interactions({ className }: { className?: ClassNameValue }) {
-  const { post } = usePost();
-  //TODO: add interactions
+	const { post } = usePost();
+	//TODO: add interactions
 
-  return (
-    <section className={cn("flex flex-row gap-4 select-none", className)}>
-      <div className="flex flex-row gap-1 items-center">
-        <PostLikeButton postId={post?.id} />
-        <span className="text-base font-semibold">{post?.likes.count}</span>
-      </div>
-    </section>
-  );
+	return (
+		<section className={cn('flex flex-row gap-4 select-none', className)}>
+			<div className="flex flex-row gap-1 items-center">
+				<PostLikeButton postId={post?.id} />
+				<span className="text-base font-semibold">{post?.likes.count}</span>
+			</div>
+		</section>
+	);
 }
 
 export function PostBody() {
-  const { post } = usePost();
+	const { post } = usePost();
 
-  return (
-    <section className="w-full flex flex-col flex-nowrap space-y-2 ">
-      <PostImageCarousel post={post} slideClassName="sm:basis-1/2" />
-      <Interactions />
-      <Description description={post?.recipe.description} />
-      <Tags tags={post?.recipe.tags} />
-    </section>
-  );
+	return (
+		<section className="w-full flex flex-col flex-nowrap space-y-2 ">
+			<PostImageCarousel post={post} slideClassName="sm:basis-1/2" />
+			<Interactions />
+			<Description description={post?.recipe.description} />
+			<Tags tags={post?.recipe.tags} />
+		</section>
+	);
 }
